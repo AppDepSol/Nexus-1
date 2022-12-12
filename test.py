a387d192f8bb9aea0f9ad06e29dc6388924a8ac0
AppDepSol <appdepsol@gmail.com>
2022-12-12 21:32:45 +0000

    Version 1

---
+++ test.py
@@ -0,0 +1,8 @@
+#In PYTHON scripting, print is used to print data
+#print("Hello-World
+#A script is a command (a collection of commands)
+#Combined scripts can be pasted at once.
+echo "Hello-World"
+echo 'Jesus is Lord'
+echo 'DevOps is truly an enviable career'
+echo "Study Devops now"
